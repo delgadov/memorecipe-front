142c7643274a7ef1717068af2a6f4ab1efe50f24
Vinicius Delgado <viniciusmardel@gmail.com>
2025-07-06 14:02:18 +1000

    style: improve the RecipeCard style

--- src/components/content/recipe/RecipeCard.vue
+++ src/components/content/recipe/RecipeCard.vue
@@ -21,15 +21,38 @@ defineProps<{
 }>();
 
 const {formatMinutesAsHoursAndMinutes} = useTimeFormat();
-
+const getDifficultyText = (level: number): string => {
+  switch (level) {
+    case 1:
+      return "Easy";
+    case 2:
+      return "Medium";
+    case 3:
+      return "Hard";
+    default:
+      return "N/A";
+  }
+}
+const getDifficultyColor = (level: number) => {
+  switch (level) {
+    case 1:
+      return "text-green-600";
+    case 2:
+      return "text-yellow-600";
+    case 3:
+      return "text-red-600";
+    default:
+      return "black";
+  }
+}
 </script>
 
 <template>
   <div class="w-full h-full flex flex-col relative ">
     <!--    Image -->
-    <div class="w-full h-64 md:h-96 z-0">
+    <div class="w-full h-64 md:h-96 z-0 rounded-tl-2xl rounded-br-2xl overflow-hidden">
       <LazyNuxtImg :src="imgUrl" alt="Recipe image for {{ recipeName }}"
-                   class="w-full h-full object-cover rounded-tl-2xl rounded-br-2xl"
+                   class="w-full h-full object-cover "
                    loading="lazy"
                    placeholder/>
     </div>
@@ -38,26 +61,39 @@ const {formatMinutesAsHoursAndMinutes} = useTimeFormat();
     <!-- Gradient overlay for readability -->
     <div class="absolute bottom-0 left-0 right-0 h-2/2 w-full z-2 pointer-events-none rounded-br-2xl"
          style="background: linear-gradient(180deg, rgba(248,248,248,0) 0%, rgba(51,51,51,0.7) 100%);"/>
+
+    <!-- Difficulty Badge -->
+    <div class="absolute top-2 left-3 px-3 py-1 rounded-full backdrop-blur-sm bg-primary-light/90">
+      <p :class="[
+        'text-xs font-bold',
+        `${getDifficultyColor(level)}`]">{{ getDifficultyText(level) }}</p>
+    </div>
+
     <!--    Overlay Information -->
     <div class="absolute left-0 bottom-0 right-0 z-3 p-4 text-white w-full flex flex-col gap-2">
       <p class="text-xl font-bold">{{ recipeName }}</p>
       <div class="text-xs flex flex-col gap-2">
-        <p class="drop-shadow-sm text-sm">{{ description }}</p>
-        <span class="inline-flex gap-4 items-center">
-          <span class="inline-flex gap-1 items-center">
-            <FireIcon/> {{ level }}
+        <p class="text-sm line-clamp-2 mb-1">{{ description }}</p>
+        <div class="flex flex-wrap gap-4 items-center">
+          <span class="inline-flex gap-1 items-center rounded-full py-1 px-2 bg-primary-light/20 backdrop-blur-sm">
+            <ClockIcon class="w-3 h-3" fill="white"/> {{ formatMinutesAsHoursAndMinutes(timeInMinutes) }}
           </span>
-          <span class="inline-flex gap-1 items-center">
-            <ClockIcon fill="white"/> {{ formatMinutesAsHoursAndMinutes(timeInMinutes) }}
+          <span class="inline-flex gap-1 items-center rounded-full py-1 px-2 bg-primary-light/20 backdrop-blur-sm">
+            <ChatIcon class="w-3 h-3" fill="white"/> {{ level }}
           </span>
-          <span class="inline-flex gap-1 items-center">
-            <ChatIcon fill="white"/> {{ level }}
-          </span>
-        </span>
+        </div>
       </div>
-      <div class="flex w-full justify-end">
-          <span
-              class="inline-flex items-center bg-primary-dark/80 text-xs font-medium rounded-full px-2 py-1 shadow-lg
+      <div class="flex w-full justify-between">
+        <div class="flex text-xs gap-4 items-center">
+          <span class="inline-flex gap-1 items-center rounded-full py-1 px-2 bg-primary-light/20 backdrop-blur-sm">
+            <PlateIcon class="w-3 h-3" fill="white"/> {{ foodType }}
+          </span>
+          <span class="inline-flex gap-1 items-center rounded-full py-1 px-2 bg-primary-light/20 backdrop-blur-sm">
+            <MapIcon class="w-3 h-3" fill="white"/> {{ country }}
+          </span>
+        </div>
+        <span
+            class="inline-flex items-center bg-primary-dark/80 text-xs font-medium rounded-full px-2 py-1 shadow-lg
                       hover:opacity-80 hover:cursor-pointer">
             View Recipe
             <RightArrow/>
@@ -65,44 +101,7 @@ const {formatMinutesAsHoursAndMinutes} = useTimeFormat();
       </div>
     </div>
 
-    <!--    <div class="grid grid-cols-2 grid-rows-[auto_auto_1fr_auto_auto] gap-2 p-4 box">-->
-    <!--      <div class="col-span-2 mb-1">-->
-    <!--        <h3 class="text-xl font-bold text-primary flex items-center mb-1">{{ recipeName }}</h3>-->
-    <!--        <p class="text-sm text-gray-600 line-clamp-2">{{ description }}</p>-->
-    <!--      </div>-->
-    <!--      <LazyClientOnly>-->
-    <!--        <div class="flex flex-col items-start gap-2">-->
-    <!--          <span class="text-xs text-gray-500">Difficulty</span>-->
-    <!--          <p aria-labelledby="level" class="flex gap-1 items-center">-->
-    <!--            <FireIcon id="level" v-for="fire in level" :key="`fire-${recipeName}-${fire}`"/>-->
-    <!--          </p>-->
-    <!--        </div>-->
-    <!--      </LazyClientOnly>-->
-    <!--      <div>-->
-    <!--        <span class="text-xs text-gray-500">Time</span>-->
-    <!--        <p class="flex gap-1 items-center text-xs text-primary">-->
-    <!--          <ClockIcon fill="black"/>-->
-    <!--          {{ formatMinutesAsHoursAndMinutes(timeInMinutes) }}-->
-    <!--        </p>-->
-    <!--      </div>-->
-    <!--      <p aria-labelledby="type" class="flex gap-1 items-center text-primary text-sm">-->
-    <!--        <PlateIcon class="mt-2"/>-->
-    <!--        {{ foodType }}-->
-    <!--      </p>-->
-    <!--      <p class="flex gap-1 items-center text-primary text-sm">-->
-    <!--        <MapIcon/>-->
-    <!--        {{ country }}-->
-    <!--      </p>-->
-    <!--      <div class="flex col-span-2 w-full justify-between">-->
-    <!--        <p class="flex gap-1 items-center text-primary text-sm">-->
-    <!--          <ChatIcon/>-->
-    <!--          {{ comments }} Comments-->
-    <!--        </p>-->
-    <!--        <p class="flex gap-1 items-center text-primary text-xs">-->
-    <!--          {{ hearts }} likes-->
-    <!--        </p>-->
-    <!--      </div>-->
-    <!--    </div>-->
+
   </div>
 </template>
 
