6491760639d2c73d9f921d3b548cfb7211bf165c
Vinicius Delgado <viniciusmardel@gmail.com>
2026-01-10 12:16:25 +1100

    update: nuxt from versio 3 to version 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  srcDir: 'src',
-  css: ['~/assets/css/main.css'],
-  modules: [
-    '@nuxtjs/tailwindcss',
-    'vue3-carousel-nuxt',
-    '@nuxt/image'
-  ],
-  tailwindcss: {
-    configPath: 'src/tailwind.config.ts',
-  }
+    compatibilityDate: '2025-05-15',
+    devtools: {enabled: true},
+    srcDir: 'src',
+    css: ['~/assets/css/main.css'],
+    modules: [
+      '@nuxtjs/tailwindcss',
+      'vue3-carousel-nuxt',
+      '@nuxt/image',
+      '@nuxt/icon'
+    ],
+    tailwindcss: {
+        configPath: 'src/tailwind.config.ts',
+    },
+    icon: {
+        mode: 'css',
+        cssLayer: 'base'
+    }
 })
